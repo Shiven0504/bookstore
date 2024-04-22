b66c91bec91658b7aa7df1a752b63810c77241f4
Shiven0504 <120889165+Shiven0504@users.noreply.github.com>
2024-04-22 18:37:32 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL = "mongodb+srv://shivengaria:shiven2754@bookstore.ctjx1em.mongodb.net/?retryWrites=true&w=majority&appName=Bookstore"
---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books/create' element={<CreateBooks />} />
+        <Route path='/books/details/:id' element={<ShowBook />} />
+        <Route path='/books/edit/:id' element={<EditBook />} />
+        <Route path='/books/delete/:id' element={<DeleteBook />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
